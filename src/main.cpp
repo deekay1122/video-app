e2246279c48fbda5ca4ed0ebb53029e247c3bf07
deekay1122 <deekay1122@gmail.com>
2020-08-16 23:54:17 +0900

    added simple rendering of red rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,25 @@ int main ()
         return 1;
     }
 
+    unsigned char* data = new unsigned char[100 * 100 * 3];
+    for (int y = 0; y < 100; ++y)
+    {
+        for (int x = 0; x < 100; ++x)
+        {
+            data[100 * y *3 + x * 3    ] = 0xff;
+            data[100 * y *3 + x * 3 + 1] = 0x00;
+            data[100 * y *3 + x * 3 + 2] = 0x00;
+        }
+    }
+
     glfwMakeContextCurrent(window);
     while (!glfwWindowShouldClose(window))
     {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glDrawPixels(100, 100, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glfwSwapBuffers(window);
         glfwWaitEvents();
     }
-    std::cout << "Hello World!" << std::endl;
 
     return 0;
 }
